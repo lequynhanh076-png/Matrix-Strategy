1422e2547dbdede8b8d72ebff449c7d8bdb59b4c
Lê Quỳnh Anh <25023150@vnu.edu.vn>
2026-05-14 13:44:10 +0700

    fix(4)

--- src/test/java/org/example/FilePathUtilsTest.java
+++ src/test/java/org/example/FilePathUtilsTest.java
@@ -1,16 +1,22 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FilePathUtilsTest {
+
     @Test
     void testPathCompatibility() {
-        String folder = "data";
-        String file = "config.txt";
-        String expected = "data/config.txt"; // Mong muốn chung cho mọi OS
 
-        // Cả 3 OS đều sẽ trả về data/config.txt sau khi đã replace
-        assertEquals(expected, FilePathUtils.getStandardPath(folder, file));
+        String expected = "data/config.txt";
+
+        String actual =
+                FilePathUtils.getStandardPath(
+                        "data",
+                        "config.txt"
+                );
+
+        assertEquals(expected, actual);
     }
 }
